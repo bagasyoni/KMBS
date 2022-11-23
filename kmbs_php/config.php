289fabe7ebf9e5c1011a63141961037423a8f780
bagasyoni <bagasyoni1997@gmail.com>
2022-11-23 10:33:17 +0700

    kmbs commit

--- kmbs_php/config.php
+++ kmbs_php/config.php
@@ -5,6 +5,6 @@ date_default_timezone_set("Asia/Bangkok");
 define('DB_NAME', 'db_kmbs');
 define('DB_USER', 'root');
 define('DB_PASSWORD', 'admin');
-define('DB_HOST', 'localhost:3309');
+define('DB_HOST', 'localhost:3306');
 
 $mysqli =  new mysqli(DB_HOST, DB_USER, DB_PASSWORD, DB_NAME);
